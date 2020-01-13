a5c21e3b627798a215ecdd0b8805679d98ab8e61
Nicole Munoz <nicolemunoz@protonmail.com>
2020-01-13 12:18:38 -0700

    initial setup

---
+++ client/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const App = props => {
+
+  return (
+    <div>hi</div>
+  )
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const webpack = require('webpack');
+module.exports = {
+  entry: './client/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  output: {
+    path: __dirname + '/public',
+    filename: 'bundle.js'
+  }
+};
